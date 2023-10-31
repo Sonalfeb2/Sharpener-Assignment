93d5d32c52f38800d490e79403fca6e77746a9e0
Sonal Agrawal <sonalfeb7@gmail.com>
2023-10-31 12:24:09 +0530

    Making Apps Error Proof

--- BackendApp/src/App.js
+++ BackendApp/src/App.js
@@ -5,18 +5,28 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [loading, setIsLoading] = useState(false);
+  const [isloading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   async function fetchHandler() {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const transformData = data.results.map(result => ({
-      id: result.episode_id,
-      title: result.title,
-      openingText: result.opening_crawl,
-      releaseDate: result.release_date
-    }));
-    setMovies(transformData);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/film/");
+      if (!response.ok) {
+        throw new Error("Something Went Wrong");
+      }
+      const data = await response.json();
+      const transformData = data.results.map(result => ({
+        id: result.episode_id,
+        title: result.title,
+        openingText: result.opening_crawl,
+        releaseDate: result.release_date
+      }));
+      setMovies(transformData);
+    } catch (error) {
+      setError(error.message);
+    }
+
     setIsLoading(false);
   }
 
@@ -26,7 +36,14 @@ function App() {
         <button onClick={fetchHandler}>Fetch Movies</button>
       </section>
       <section>
-        {loading ? <p>Loading...</p> : <MoviesList movies={movies} />}
+        {!isloading && movies.length>0 && !error && <MoviesList />}
+        {!isloading && movies.length===0  && !error && <p> Movies not found </p>}
+        {isloading && <p>Loading...</p>
+        }
+        {!isloading && error &&
+          <p>
+            {error}
+          </p>}
       </section>
     </React.Fragment>
   );
